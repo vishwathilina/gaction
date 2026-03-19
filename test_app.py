e2997a793733cc5ec40a2bd1ff462b6e0bb85253
vishwathilina <vishwathilina@outlook.com>
2026-03-19 14:55:11 +0530

    app

---
+++ test_app.py
@@ -0,0 +1,6 @@
+from app import app
+def test_hello():
+    client = app.test_client()
+    response = client.get('/')
+    assert response.status_code == 200
+    assert response.data == b'Hello, World!' in response.data
